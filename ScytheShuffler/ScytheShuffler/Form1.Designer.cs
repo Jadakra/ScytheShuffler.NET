f2e63b6a0c844e07c0a84a8104e2567c006d9c04
Jadakra <Jadakra@users.noreply.github.com>
2019-07-09 13:04:38 -0700

    Initial Commit
    I plan to do this on Android, just needed to visualize how

---
+++ ScytheShuffler/ScytheShuffler/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace ScytheShuffler
+{
+    partial class ScytheShuffler
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DoItButton = new System.Windows.Forms.Button();
+            this.Results = new System.Windows.Forms.TextBox();
+            this.Players = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // DoItButton
+            // 
+            this.DoItButton.AutoSize = true;
+            this.DoItButton.Location = new System.Drawing.Point(269, 370);
+            this.DoItButton.Name = "DoItButton";
+            this.DoItButton.Size = new System.Drawing.Size(75, 23);
+            this.DoItButton.TabIndex = 0;
+            this.DoItButton.Text = "Do it";
+            this.DoItButton.UseVisualStyleBackColor = true;
+            this.DoItButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Results
+            // 
+            this.Results.Location = new System.Drawing.Point(13, 13);
+            this.Results.Multiline = true;
+            this.Results.Name = "Results";
+            this.Results.Size = new System.Drawing.Size(713, 329);
+            this.Results.TabIndex = 1;
+            // 
+            // Players
+            // 
+            this.Players.FormattingEnabled = true;
+            this.Players.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.Players.Location = new System.Drawing.Point(588, 372);
+            this.Players.Name = "Players";
+            this.Players.Size = new System.Drawing.Size(121, 21);
+            this.Players.TabIndex = 3;
+            this.Players.Text = "5";
+            // 
+            // ScytheShuffler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Players);
+            this.Controls.Add(this.Results);
+            this.Controls.Add(this.DoItButton);
+            this.Name = "ScytheShuffler";
+            this.Text = "Scythe Shuffler";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DoItButton;
+        private System.Windows.Forms.TextBox Results;
+        private System.Windows.Forms.ComboBox Players;
+    }
+}
+
---
+++ ScytheShuffler/ScytheShuffler/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ScytheShuffler
+{
+    public partial class ScytheShuffler : Form
+    {
+        string[] Factions = new string[5] { "Crimea", "Nordic", "Polania", "Rusviet", "Saxony" };//"Albion","Togawa"
+        string[] PlayMats = new string[5] { "Industrial", "Engineering", "Patriotic", "Mechanical", "Agricultural" }; //Militant 2.5, Innovative 3.5, 
+        int found = -1;
+
+        Random random = new Random();
+
+        public ScytheShuffler()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Results.Text = "";
+            int numberOfPlayers = Convert.ToInt32(Players.SelectedItem);
+            int minF = 1, maxF = 5, minP = 1, maxP = 5;
+            //int[] usedFactions = new int[numberOfPlayers];
+            int[] usedFactions = new int[5] { -1, -1, -1, -1, -1 };
+            int[] usedPlaymats = new int[numberOfPlayers];
+            for (int i = 1; i <= numberOfPlayers; i++)
+            {
+                int newRandomFaction = RandomNumber(minF, maxF);
+                while (Array.IndexOf(usedFactions, newRandomFaction) != found)
+                {
+
+                    newRandomFaction = RandomNumber(minF, maxF);
+                    if (Array.IndexOf(usedFactions, minF) != -1)
+                    { minF++; }
+                    if (Array.IndexOf(usedFactions, maxF) != -1)
+                    { maxF--; }
+                }
+                usedFactions[i - 1] = newRandomFaction;
+                int newRandomPlaymat = RandomNumber(1, 5);
+                while (Array.IndexOf(usedPlaymats, newRandomPlaymat) != found)
+                {
+                    newRandomPlaymat = RandomNumber(minP, maxP);
+                    if (Array.IndexOf(usedPlaymats, minP) != -1)
+                    { minP++; }
+                    if (Array.IndexOf(usedPlaymats, maxP) != -1)
+                    { maxP--; }
+                }
+                usedPlaymats[i - 1] = newRandomPlaymat;
+                //Consigue dos listados igual al numero de jugadores
+                //Que dentro del listado individual no haya repeticiones
+                //Asigna esos numeros a los arreglos
+
+                Results.Text += Factions[newRandomFaction - 1] + "," + PlayMats[newRandomPlaymat - 1] + Environment.NewLine;
+                //Results.Text += randomFaction + "," + randomPlayMat + Environment.NewLine;
+            }
+
+
+        }
+        public int RandomNumber(int min, int max)
+        {
+
+            return random.Next(min, max);
+        }
+
+        void Start()
+        {
+        }
+    }
+}
